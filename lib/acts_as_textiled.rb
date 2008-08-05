b96cc20452be36312dd2a06f546868707169d549
Lawrence Pit <lawrence.pit@gmail.com>
2008-08-05 13:10:35 +0800

    only apply CGI.escapeHTML to script stuff.
    Signed-off-by: Chris Wanstrath <chris@ozmm.org>

--- lib/acts_as_textiled.rb
+++ lib/acts_as_textiled.rb
@@ -26,17 +26,23 @@ module Err
               if type.nil? && self[attribute]
                 t = textiled[attribute.to_s]
                 if t.nil?
-                  linked = auto_link(CGI.escapeHTML(self[attribute]), :all) do |txt|
+                  str = self[attribute]
+
+                  # suck out script stuff
+                  str.gsub!(/\<(java)?script\>(.*?)\<\/(java)?script\>/i) do |match|
+                    CGI.escapeHTML(match)
+                  end
+
+                  str = auto_link(str, :all) do |txt|
                     txt.size < 55 ? txt : truncate(txt, 50)
                   end
-                  
-                  # &lt;a href='http://domain.com'&gt;some text&lt;/a&gt;
-                  linked.sub!(/\&lt\;a href=([\'\"].*?[\'\"])\&gt\;(.*?)\&lt\;\/a\&gt\;/, '<a href=\1>\2</a>')
-                  
-                  t = RedCloth.new(linked, Array(ruled[attribute])).to_html
+
+                  str = RedCloth.new(str, Array(ruled[attribute])).to_html
+
                   # preserve whitespace for haml
-                  t = t.chomp("\n").gsub(/\n/, '&#x000A;').gsub(/\r/, '')
-                  textiled[attribute.to_s] = t
+                  str = str.chomp("\n").gsub(/\n/, '&#x000A;').gsub(/\r/, '')
+
+                  textiled[attribute.to_s] = t = str
                 end
                 t
               elsif type.nil? && self[attribute].nil?
